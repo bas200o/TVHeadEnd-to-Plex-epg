100b2161267f108088abc49a7a5081523f7eb205
bas <bas.winkelhof@hotmail.nl>
2020-06-04 14:49:03 +0200

    change config to ubuntu file system added user agent

--- src/TVHApi/ChannelsApi.java
+++ src/TVHApi/ChannelsApi.java
@@ -78,6 +78,7 @@ public class ChannelsApi {
 
         HttpURLConnection connection = (HttpURLConnection) tvhurl.openConnection();
         connection.setRequestMethod("GET");
+        connection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
 
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(connection.getInputStream()));
